60ae267c32ae3d11c172d72fb4bbf9e784d4f057
Graziano Giuliano Roccon <g.roccon@agos.it>
2019-09-23 09:18:57 +0200

    Version 3 - Change way to send date to the radio

--- Set_IC7300_time-args-ITA.py
+++ Set_IC7300_time-args-ITA.py
@@ -96,19 +96,19 @@ else:
     day = today.strftime("%d")
 
     # Preparing hexcode for today date
-    preyear = "0x20" 
+    #preyear = "0x20" 
     year = "0x" + str(year)
     month = "0x" + str(month)
     day = "0x" + str(day)
     # print( year + " " + month + " " + day)
 
-    preambledate.append(preyear)
+    #preambledate.append(preyear)
     preambledate.append(year)
     preambledate.append(month)
     preambledate.append(day)
     preambledate.append('0xFD')
     #print(" ")
-    #print("Preamble date: " + str(preambledate))
+    print("Preamble date: " + str(preambledate))
     #print(" ")
 
     # End section for date
@@ -128,7 +128,7 @@ else:
     
     # Send date after time
     count = 0
-    while(count < 13):
+    while(count < 12):
         senddata2 = int(bytes(preambledate[count], 'UTF-8'), 16)
         #Aggiunta di IW2NOY per debugging dei dati inviati
         #dati = str(senddata2)
